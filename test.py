4bb433b685f8d9dca18951c1fe2e1c763f677957
albecabrera <albe.cabrera@yahoo.de>
2025-02-22 23:06:41 +0100

    second commit

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ numero2 = int(input("Introduce el segundo número"))
 resultado = numero1 + numero2
 
 print("El resultado de la suma es: ", resultado)
+
+name = "Alberto"
+age = 47
+
+print(f"El nombre es: {name} y tiene {age} años.")
