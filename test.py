5c60737748497349524d3ad4acb7a35892e5dd9a
albecabrera <albe.cabrera@yahoo.de>
2025-02-22 22:09:39 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+# Ejercicio básico para hacer una calculadora
+
+numero1 = int(input("Introduce el primer número"))
+numero2 = int(input("Introduce el segundo número"))
+
+resultado = numero1 + numero2
+
+print("El resultado de la suma es: ", resultado)
