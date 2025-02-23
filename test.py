80248b9dae59db26181aef45fc6865e03ad0aa39
albecabrera <albe.cabrera@yahoo.de>
2025-02-23 23:01:28 +0100

    tercer cambio

---
+++ test.py
@@ -0,0 +1,13 @@
+# Ejercicio básico para hacer una calculadora
+
+numero1 = int(input("Introduce el primer número"))
+numero2 = int(input("Introduce el segundo número"))
+
+resultado = numero1 + numero2
+
+print("El resultado de la suma es: ", resultado)
+
+name = "Alberto"
+age = 47
+
+print(f"El nombre es: {name} y tiene {age} años.")
